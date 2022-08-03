4f52a7e5b59c6e0aff277518580a708380f211b7
Osvaldo da Silva Ornelas Neto <osvaldo.ornelas3@gmail.com>
2022-08-03 04:43:22 -0500

    Provisionamento

---
+++ provisionamento/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
---
+++ provisionamento/vars.tf
@@ -0,0 +1,16 @@
+variable "client_id" {
+  type = string
+  default = "40e2cba4-e391-4377-9575-56f51aa04f77"
+}
+variable "client_secret" {
+  type = string
+  default = "40282f38-a415-43db-bc57-2106f0c62d11"
+}
+variable "tenant_id" {
+  type = string
+  default = "07641565-168f-4a13-a690-f7a9313c7b9d"
+}
+
+variable "subscription_id" {
+  type = string
+  default = "465da16b-f182-40ed-9e45-ddb9a81a701f"
